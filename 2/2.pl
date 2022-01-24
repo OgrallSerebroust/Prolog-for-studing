cb2b67bc2228912ea921fca0ba4d96447c4b026c
Ograll <stefplotva@gmail.com>
2022-01-24 15:13:45 +0300

    Data added

--- 2/2.pl
+++ 2/2.pl
@@ -1,1 +1,244 @@
-/* Р—Р°РґР°РЅРёРµ 1 */
+galaxyLevel("Млечный путь", 1).
+galaxyLevel("Солнечная", 0).
+galaxyLevel("Солнце", 0).
+galaxyLevel("Земля", 0).
+galaxyLevel("Юпитер", 0).
+galaxyLevel("Плутон", 0).
+galaxyLevel("Галлея", 0).
+galaxyLevel("Кеплер-419с", 0).
+galaxyLevel("GD 165B", 0).
+galaxyLevel("Альциона", 0).
+galaxyLevel("Ригель", 0).
+galaxyLevel("NGC 300 X-1", 0).
+galaxyLevel("Бета Живописца b", 0).
+galaxyLevel("HD 29587 b", 0).
+galaxyLevel("Тубан", 0).
+galaxyLevel("Поллукс", 0).
+galaxyLevel("Бетельгейзе", 0).
+galaxyLevel("Полярная", 0).
+galaxyLevel("Сатурн", 0).
+galaxyLevel("Эрида", 0).
+systemLevel("Млечный путь", 0).
+systemLevel("Солнечная", 1).
+systemLevel("Солнце", 0).
+systemLevel("Земля", 0).
+systemLevel("Юпитер", 0).
+systemLevel("Плутон", 0).
+systemLevel("Галлея", 0).
+systemLevel("Кеплер-419с", 0).
+systemLevel("GD 165B", 0).
+systemLevel("Альциона", 0).
+systemLevel("Ригель", 0).
+systemLevel("NGC 300 X-1", 0).
+systemLevel("Бета Живописца b", 0).
+systemLevel("HD 29587 b", 0).
+systemLevel("Тубан", 0).
+systemLevel("Поллукс", 0).
+systemLevel("Бетельгейзе", 0).
+systemLevel("Полярная", 0).
+systemLevel("Сатурн", 0).
+systemLevel("Эрида", 0).
+objectLevel("Млечный путь", 0).
+objectLevel("Солнечная", 0).
+objectLevel("Солнце", 1).
+objectLevel("Земля", 1).
+objectLevel("Юпитер", 1).
+objectLevel("Плутон", 1).
+objectLevel("Галлея", 1).
+objectLevel("Кеплер-419c", 1).
+objectLevel("GD 165B", 1).
+objectLevel("Альциона", 1).
+objectLevel("Ригель", 1).
+objectLevel("NGC 300 X-1", 1).
+objectLevel("Бета Живописца b", 1).
+objectLevel("HD 29587 b", 1).
+objectLevel("Тубан", 1).
+objectLevel("Поллукс", 1).
+objectLevel("Бетельгейзе", 1).
+objectLevel("Полярная", 1).
+objectLevel("Сатурн", 1).
+objectLevel("Эрида", 1).
+hasSpecterClass("Млечный путь", 0).
+hasSpecterClass("Солнечная", 0).
+hasSpecterClass("Солнце", 1).
+hasSpecterClass("Земля", 0).
+hasSpecterClass("Юпитер", 0).
+hasSpecterClass("Плутон", 0).
+hasSpecterClass("Галлея", 0).
+hasSpecterClass("Кеплер-419c", 0).
+hasSpecterClass("GD 165B", 1).
+hasSpecterClass("Альциона", 1).
+hasSpecterClass("Ригель", 1).
+hasSpecterClass("NGC 300 X-1", 0).
+hasSpecterClass("Бета Живописца b", 0).
+hasSpecterClass("HD 29587 b", 0).
+hasSpecterClass("Тубан", 1).
+hasSpecterClass("Поллукс", 1).
+hasSpecterClass("Бетельгейзе", 1).
+hasSpecterClass("Полярная", 1).
+hasSpecterClass("Сатурн", 0).
+hasSpecterClass("Эрида", 0).
+isEarthLike("Млечный путь", 0).
+isEarthLike("Солнечная", 0).
+isEarthLike("Солнце", 0).
+isEarthLike("Земля", 1).
+isEarthLike("Юпитер", 0).
+isEarthLike("Плутон", 0).
+isEarthLike("Галлея", 0).
+isEarthLike("Кеплер-419c", 0).
+isEarthLike("GD 165B", 0).
+isEarthLike("Альциона", 0).
+isEarthLike("Ригель", 0).
+isEarthLike("NGC 300 X-1", 0).
+isEarthLike("Бета Живописца b", 0).
+isEarthLike("HD 29587 b", 0).
+isEarthLike("Тубан", 0).
+isEarthLike("Поллукс", 0).
+isEarthLike("Бетельгейзе", 0).
+isEarthLike("Полярная", 0).
+isEarthLike("Сатурн", 0).
+isEarthLike("Эрида", 0).
+hugeMass("Млечный путь", 1).
+hugeMass("Солнечная", 1).
+hugeMass("Солнце", 1).
+hugeMass("Земля", 0).
+hugeMass("Юпитер", 1).
+hugeMass("Плутон", 0).
+hugeMass("Галлея", 0).
+hugeMass("Кеплер-419c", 1).
+hugeMass("GD 165B", 1).
+hugeMass("Альциона", 1).
+hugeMass("Ригель", 1).
+hugeMass("NGC 300 X-1", 1).
+hugeMass("Бета Живописца b", 1).
+hugeMass("HD 29587 b", 1).
+hugeMass("Тубан", 1).
+hugeMass("Поллукс", 1).
+hugeMass("Бетельгейзе", 1).
+hugeMass("Полярная", 1).
+hugeMass("Сатурн", 1).
+hugeMass("Эрида", 0).
+smallMass("Млечный путь", 0).
+smallMass("Солнечная", 0).
+smallMass("Солнце", 0).
+smallMass("Земля", 0).
+smallMass("Юпитер", 0).
+smallMass("Плутон", 0).
+smallMass("Галлея", 1).
+smallMass("Кеплер-419c", 0).
+smallMass("GD 165B", 0).
+smallMass("Альциона", 0).
+smallMass("Ригель", 0).
+smallMass("NGC 300 X-1", 0).
+smallMass("Бета Живописца b", 0).
+smallMass("HD 29587 b", 0).
+smallMass("Тубан", 0).
+smallMass("Поллукс", 0).
+smallMass("Бетельгейзе", 0).
+smallMass("Полярная", 0).
+smallMass("Сатурн", 0).
+smallMass("Эрида", 0).
+sunSatellite("Млечный путь", 0).
+sunSatellite("Солнечная", 0).
+sunSatellite("Солнце", 0).
+sunSatellite("Земля", 1).
+sunSatellite("Юпитер", 1).
+sunSatellite("Плутон", 1).
+sunSatellite("Галлея", 0).
+sunSatellite("Кеплер-419c", 0).
+sunSatellite("GD 165B", 0).
+sunSatellite("Альциона", 0).
+sunSatellite("Ригель", 0).
+sunSatellite("NGC 300 X-1", 0).
+sunSatellite("Бета Живописца b", 0).
+sunSatellite("HD 29587 b", 0).
+sunSatellite("Тубан", 0).
+sunSatellite("Поллукс", 0).
+sunSatellite("Бетельгейзе", 0).
+sunSatellite("Полярная", 0).
+sunSatellite("Сатурн", 1).
+sunSatellite("Эрида", 1).
+nucleoble("Млечный путь", 0).
+nucleoble("Солнечная", 0).
+nucleoble("Солнце", 1).
+nucleoble("Земля", 0).
+nucleoble("Юпитер", 0).
+nucleoble("Плутон", 0).
+nucleoble("Галлея", 0).
+nucleoble("Кеплер-419c", 0).
+nucleoble("GD 165B", 0).
+nucleoble("Альциона", 1).
+nucleoble("Ригель", 1).
+nucleoble("NGC 300 X-1", 0).
+nucleoble("Бета Живописца b", 0).
+nucleoble("HD 29587 b", 0).
+nucleoble("Тубан", 1).
+nucleoble("Поллукс", 1).
+nucleoble("Бетельгейзе", 1).
+nucleoble("Полярная", 1).
+nucleoble("Сатурн", 0).
+nucleoble("Эрида", 0).
+tenRadiuses("Млечный путь", 0).
+tenRadiuses("Солнечная", 0).
+tenRadiuses("Солнце", 0).
+tenRadiuses("Земля", 0).
+tenRadiuses("Юпитер", 0).
+tenRadiuses("Плутон", 0).
+tenRadiuses("Галлея", 0).
+tenRadiuses("Кеплер-419c", 0).
+tenRadiuses("GD 165B", 0).
+tenRadiuses("Альциона", 1).
+tenRadiuses("Ригель", 0).
+tenRadiuses("NGC 300 X-1", 0).
+tenRadiuses("Бета Живописца b", 0).
+tenRadiuses("HD 29587 b", 0).
+tenRadiuses("Тубан", 1).
+tenRadiuses("Поллукс", 1).
+tenRadiuses("Бетельгейзе", 0).
+tenRadiuses("Полярная", 0).
+tenRadiuses("Сатурн", 0).
+tenRadiuses("Эрида", 0).
+luminosityClassI("Млечный путь", 0).
+luminosityClassI("Солнечная", 0).
+luminosityClassI("Солнце", 0).
+luminosityClassI("Земля", 0).
+luminosityClassI("Юпитер", 0).
+luminosityClassI("Плутон", 0).
+luminosityClassI("Галлея", 0).
+luminosityClassI("Кеплер-419c", 0).
+luminosityClassI("GD 165B", 0).
+luminosityClassI("Альциона", 0).
+luminosityClassI("Ригель", 1).
+luminosityClassI("NGC 300 X-1", 0).
+luminosityClassI("Бета Живописца b", 0).
+luminosityClassI("HD 29587 b", 0).
+luminosityClassI("Тубан", 0).
+luminosityClassI("Поллукс", 0).
+luminosityClassI("Бетельгейзе", 1).
+luminosityClassI("Полярная", 1).
+luminosityClassI("Сатурн", 0).
+luminosityClassI("Эрида", 0).
+greatGravitationalAttraction("Млечный путь", 0).
+greatGravitationalAttraction("Солнечная", 0).
+greatGravitationalAttraction("Солнце", 0).
+greatGravitationalAttraction("Земля", 0).
+greatGravitationalAttraction("Юпитер", 0).
+greatGravitationalAttraction("Плутон", 0).
+greatGravitationalAttraction("Галлея", 0).
+greatGravitationalAttraction("Кеплер-419c", 0).
+greatGravitationalAttraction("GD 165B", 0).
+greatGravitationalAttraction("Альциона", 0).
+greatGravitationalAttraction("Ригель", 0).
+greatGravitationalAttraction("NGC 300 X-1", 1).
+greatGravitationalAttraction("Бета Живописца b", 0).
+greatGravitationalAttraction("HD 29587 b", 0).
+greatGravitationalAttraction("Тубан", 0).
+greatGravitationalAttraction("Поллукс", 0).
+greatGravitationalAttraction("Бетельгейзе", 0).
+greatGravitationalAttraction("Полярная", 0).
+greatGravitationalAttraction("Сатурн", 0).
+greatGravitationalAttraction("Эрида", 0).
+
+question1(X1) :- write("Это больше уровня системы?"), nl, read(X1).
+
+answer :- question1(X1), galaxyLevel(X, X1), write(X).
